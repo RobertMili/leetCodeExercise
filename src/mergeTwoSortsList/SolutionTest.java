c06cde55d9b72141dd337b553b96e5b5b370da60
RobertMili <98566676+RobertMili@users.noreply.github.com>
2023-02-05 14:05:55 +0100

    added SOlutionTest focus on metod: mergeTwoList

--- src/mergeTwoSortsList/SolutionTest.java
+++ src/mergeTwoSortsList/SolutionTest.java
@@ -7,33 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolutionTest {
 
     Solution solution = new Solution();
+
     @Test
     void mergeCheckingIfIsWork() {
 
         ListNode list1 = new ListNode(1);
         ListNode list2 = new ListNode(2);
-        var adding = solution.mergeTwoLists(list1,list2);
+        var adding = solution.mergeTwoLists(list1, list2);
 
-        var result = new ListNode(1,new ListNode(2));
+        var result = new ListNode(1, new ListNode(2));
 
-        assertEquals(result.val,adding.val);
+        assertEquals(result.val, adding.val);
     }
+
     @Test
-    void addingTwoList() {
+    void adding4Numbers() {
 
-        ListNode list1 = new ListNode(1, new ListNode(1, new ListNode(4)));
-        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-        ListNode result = new Solution().mergeTwoLists(list1, list2);
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
 
-        ListNode expected = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))));
 
-        System.out.println(expected.toString());
-        while (result != null && expected != null) {
-            assertEquals(expected.val, result.val);
-            expected = expected.next;
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(2);
+        list2.next.next = new ListNode(3);
+
+        ListNode expectedResult = new ListNode(1);
+        expectedResult.next = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(3))));
+
+        ListNode result = solution.mergeTwoLists(list1, list2);
+
+        while (result != null) {
+            assertEquals(expectedResult.val, result.val);
+            expectedResult = expectedResult.next;
             result = result.next;
         }
 
-
     }
+
+
 }
