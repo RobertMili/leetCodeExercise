fb8eaa54c80b47e8a4d0c87262c7195d80372458
RobertMili <robertmilicevic3869@gmail.com>
2024-09-23 15:28:54 +0200

    add prepairing for tech interviu

--- src/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.java
+++ src/bestTimeToBuyAndSellStock/bestTimeToBuyAndSellStock.java
@@ -1,4 +1,35 @@
 package bestTimeToBuyAndSellStock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class bestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nb = {7, 1, 5, 3, 6, 4};
+        solution.maxProfit(nb);
+    }
 }
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0 || prices == null) {
+            return 0;
+        }
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        System.out.println(maxProfit);
+
+        return maxProfit;
+    }
+}
+
