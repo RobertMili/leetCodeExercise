7bdb5c33e5d5c460d2ff23a554330a41227b53a6
RobertMili <robertmilicevic3869@gmail.com>
2024-09-25 13:28:41 +0200

    this is passing a 41 test

--- src/Kattis/ProblemCTest.java
+++ src/Kattis/ProblemCTest.java
@@ -50,53 +50,6 @@
 //        assertEquals(expectedOutput, actualOutput);
 //    }
 //
-//    @Test
-//    void testExample3() {
-//        String input = "4 4\n";
-//        String expectedOutput =
-//                " _ _ _ _ \n" +
-//                        "| | | | |\n" +
-//                        "|_ _| | |\n" +
-//                        "|_ _  | |\n" +
-//                        "|_ _|_ _|\n";
 //
-//        String actualOutput = runProblemC(input);
-//        assertEquals(expectedOutput, actualOutput);
-//    }
 //
-//    @Test
-//    void testSingleCell() {
-//        String input = "1 1\n";
-//        String expectedOutput =
-//                " _ \n" +
-//                        "|_|\n";
-//
-//        String actualOutput = runProblemC(input);
-//        assertEquals(expectedOutput, actualOutput);
-//    }
-//
-//    @Test
-//    void testSingleRow() {
-//        String input = "1 3\n";
-//        String expectedOutput =
-//                " _ _ _ \n" +
-//                        "| | | |\n" +
-//                        "|_|_|_|\n";
-//
-//        String actualOutput = runProblemC(input);
-//        assertEquals(expectedOutput, actualOutput);
-//    }
-//
-//    @Test
-//    void testSingleColumn() {
-//        String input = "3 1\n";
-//        String expectedOutput =
-//                " _ \n" +
-//                        "| |\n" +
-//                        "| |\n" +
-//                        "|_|\n";
-//
-//        String actualOutput = runProblemC(input);
-//        assertEquals(expectedOutput, actualOutput);
-//    }
 //}
